905541e16e6523516202a0b02ba268066151dd0a
Alik <alik@gmail.com>
2025-02-06 11:43:53 +0630

    New files added

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound(){
+    return (
+        <>
+            <h1>Page not found</h1>
+            <p>Cound not find requested resource</p>
+        </>
+    )
+}
---
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -0,0 +1,15 @@
+import { notFound } from "next/navigation";
+
+export default function ReviewDetails( { params }:
+    {
+        params: {
+            productId: string,
+            reviewId: string
+        }
+    }
+){
+    if(parseInt(params.reviewId) > 1000){
+        notFound();
+    }
+    return <h1>Review {params.reviewId} for product {params.productId}</h1>
+}
