1c07f40537f402327dd9dc3fd857fd8f6e3c2933
Alik <taezarlin1@gmail.com>
2025-02-15 09:42:30 +0630

    Dynamic routes

---
+++ src/app/products/[productId]/page.tsx
@@ -0,0 +1,6 @@
+export default async function ProductDetails({params}:{
+    params: Promise<{productId: string}>
+}) {
+    const productId = (await params).productId;
+    return <h2>Details about product {productId}</h2>
+}
