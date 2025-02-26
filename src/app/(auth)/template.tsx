fe43854f39251e48c8b921b8b3489597ef682764
Alik <taezarlin1@gmail.com>
2025-02-26 10:06:31 +0630

    Templates

--- src/app/(auth)/template.tsx
+++ src/app/(auth)/template.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import "./styles.css"
+import { useState } from "react";
 
 const navLinks = [
     { name: "Register", href: "/register" },
@@ -13,13 +14,22 @@ const navLinks = [
 export default function AuthLayout({children}:{
     children: React.ReactNode
 }){
+    const [input, setInput] = useState("");
     const pathname = usePathname();
     return (
         <>
+            <div>
+                <input value={input} onChange={(e)=> setInput(e.target.value)} />
+            </div>
             {navLinks.map((link)=>{
                 const isActive = pathname === link.href || (pathname.startsWith(link.href) && link.href !== "/");
                 return (
-                    <Link className={isActive ? "font-bold mr-4" : "text-blue-500 mr-4"} href={link.href} key={link.name}>{link.name}</Link>
+                    <Link 
+                        className={isActive ? "font-bold mr-4" : "text-blue-500 mr-4"} 
+                        href={link.href} 
+                        key={link.name}>
+                            {link.name}
+                    </Link>
                 )
             })}
             {children}
--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,11 +1,12 @@
-import { notFound } from "next/navigation";
+import { notFound, redirect } from "next/navigation";
 
 export default async function ReviewDetails({params}:{
     params: Promise<{productId: string, reviewId: string}>
 }) {
     const {productId, reviewId} = await params;
     if (parseInt(reviewId) > 1000){
-        notFound();
+        // notFound();
+        redirect("/products")
     }
     return <h2>Review {reviewId} for Product {productId}</h2>
 }
