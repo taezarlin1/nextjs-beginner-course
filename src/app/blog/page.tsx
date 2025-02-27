241af963efd818ce18e1d1c7ee9dbe4b339c7d8e
Alik <taezarlin1@gmail.com>
2025-02-27 21:14:03 +0630

    added

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,16 @@
-export default function Blog(){
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: {
+        absolute: "Blog"
+    }
+}
+
+export default async function Blog(){
+    await new Promise(resolve=>{
+        setTimeout(()=>{
+            resolve("intentional delay");
+        },2000)
+    })
     return <h1>Blog Page</h1>
 }
