7c23248fa73166f1cf9db5e50452f68a373fe94d
Alik <taezarlin1@gmail.com>
2025-02-28 07:59:40 +0630

    Error handling

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,8 +1,17 @@
+import { error } from "console";
 import { notFound, redirect } from "next/navigation";
+
+function getRandomInt(count: number){
+    return Math.floor(Math.random() * count);
+}
 
 export default async function ReviewDetails({params}:{
     params: Promise<{productId: string, reviewId: string}>
 }) {
+    const random = getRandomInt(2);
+    if(random===1){
+        throw new Error("Error Loading Review");
+    }
     const {productId, reviewId} = await params;
     if (parseInt(reviewId) > 1000){
         // notFound();
