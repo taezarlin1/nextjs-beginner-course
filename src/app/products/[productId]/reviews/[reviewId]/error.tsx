ffc041a621ea89239e39d595647dc7c15f109bfc
Alik <taezarlin1@gmail.com>
2025-02-28 08:06:08 +0630

    Error handling

--- src/app/products/[productId]/reviews/[reviewId]/error.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -1,7 +1,23 @@
 "use client";
 
-export default function ErrorBoundary({error}:{
+import { useRouter } from "next/navigation";
+import { startTransition } from "react";
+
+export default function ErrorBoundary({error, reset}:{
     error: Error
+    reset: () => void;
 }){
-    return <div>{error.message}</div>
+    const router = useRouter();
+    const reload = ()=>{
+        startTransition(()=>{
+            router.refresh();
+            reset();
+        })
+    }
+    return (
+    <div>
+        <p>{error.message}</p>
+        <button onClick={reload}>Try again</button>
+    </div>
+    )
 }
