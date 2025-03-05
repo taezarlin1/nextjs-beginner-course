7613f768297265b2651bfc9082b222e4d8d14923
Alik <taezarlin1@gmail.com>
2025-03-05 07:46:27 +0630

    Parallel routes

--- src/app/complex-dashboard/layout.tsx
+++ src/app/complex-dashboard/layout.tsx
@@ -1,9 +1,16 @@
+import Notifications from "./@notifications/page"
+import RevenueMetrics from "./@revenue/page"
+import UsersAnalytics from "./@users/page"
+
 export default function ComplexDashboardLayout({children}:{
     children: React.ReactNode
 }){
     return (
         <>
             <div>{children}</div>
+            <UsersAnalytics/>
+            <RevenueMetrics/>
+            <Notifications/>
         </>
     )
 }
